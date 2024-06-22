0e57e9d630eab9ab6b4848e6294ec8c1fef68240
BCCO1510 <brahiam2022@gmail.com>
2024-06-21 23:51:41 -0500

    first commit

---
+++ product_invent_ionic/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'product_invent_ionic',
+  webDir: 'www'
+};
+
+export default config;
---
+++ product_invent_ionic/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDIHyTJnBk1BQUmvXmjihuqzdZsao1waeA",
+    authDomain: "product-invent-app.firebaseapp.com",
+    projectId: "product-invent-app",
+    storageBucket: "product-invent-app.appspot.com",
+    messagingSenderId: "567340563873",
+    appId: "1:567340563873:web:53f9f8ba61b727fb891fe0"
+  }
+};
